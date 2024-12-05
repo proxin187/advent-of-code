449062b52ec883a4a50cada9a62f717146db43b9
proxin187 <thugshaker627@gmail.com>
2024-12-05 09:41:22 +0100

    feat: day4 done

--- 2024/day4/src/main.rs
+++ 2024/day4/src/main.rs
@@ -3,6 +3,12 @@ use std::fs;
 const DIRECTIONS: [Direction; 4] = [Direction::Up, Direction::Down, Direction::Right, Direction::Left];
 const PLANES: [Plane; 2] = [Plane::Straight, Plane::Diagonal];
 
+const NEEDLE: [char; 4] = ['X', 'M', 'A', 'S'];
+const REVERSE: [char; 4] = ['S', 'A', 'M', 'X'];
+
+const MAS_NEEDLE: [char; 3] = ['M', 'A', 'S'];
+const MAS_REVERSE: [char; 3] = ['S', 'A', 'M'];
+
 
 #[derive(Debug, Clone, Copy)]
 pub enum Direction {
@@ -18,61 +24,56 @@ pub enum Plane {
     Diagonal,
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Clone, Copy)]
 pub struct Position {
     x: usize,
     y: usize,
 }
 
-pub struct Word {
+pub struct Part1 {
     columns: Vec<Vec<char>>,
     pos: Position,
 }
 
-impl Word {
-    pub fn new(file: &str) -> Result<Word, Box<dyn std::error::Error>> {
+impl Part1 {
+    pub fn new(file: &str) -> Result<Part1, Box<dyn std::error::Error>> {
         let content = fs::read_to_string(file)?;
 
         let columns = content.lines()
             .map(|line| line.chars().collect::<Vec<char>>())
             .collect::<Vec<Vec<char>>>();
 
-        Ok(Word {
+        Ok(Part1 {
             columns,
             pos: Position::default(),
         })
     }
 
     fn scan(&self, direction: Direction, plane: Plane) -> bool {
-        let needle = ['X', 'M', 'A', 'S'];
-
-        // TODO: we also need to check in reverse here, for example reverse on line two are not
-        // detected
-
         let result = match plane {
             Plane::Straight => match direction {
                 Direction::Up => self.pos.y >= 3
-                    && self.columns[self.pos.y - 3..=self.pos.y].iter().map(|column| column[self.pos.x]).collect::<Vec<char>>() == needle,
+                    && self.columns[self.pos.y - 3..=self.pos.y].iter().map(|column| column[self.pos.x]).collect::<Vec<char>>() == REVERSE,
                 Direction::Down => self.pos.y + 3 < self.columns.len()
-                    && self.columns[self.pos.y..self.pos.y + 4].iter().map(|column| column[self.pos.x]).collect::<Vec<char>>() == needle,
+                    && self.columns[self.pos.y..self.pos.y + 4].iter().map(|column| column[self.pos.x]).collect::<Vec<char>>() == NEEDLE,
                 Direction::Right => self.pos.x + 3 < self.columns[self.pos.y].len()
-                    && self.columns[self.pos.y][self.pos.x..self.pos.x + 4] == needle,
+                    && self.columns[self.pos.y][self.pos.x..self.pos.x + 4] == NEEDLE,
                 Direction::Left => self.pos.x >= 3
-                    && self.columns[self.pos.y][self.pos.x - 3..=self.pos.x] == needle,
+                    && self.columns[self.pos.y][self.pos.x - 3..=self.pos.x] == REVERSE,
             },
             Plane::Diagonal => match direction {
                 Direction::Up => self.pos.y >= 3
                     && self.pos.x + 3 < self.columns[self.pos.y].len()
-                    && self.columns[self.pos.y - 3..=self.pos.y].iter().enumerate().map(|(count, column)| column[self.pos.x + count]).collect::<Vec<char>>() == needle,
+                    && self.columns[self.pos.y - 3..=self.pos.y].iter().enumerate().map(|(count, column)| column[self.pos.x + count]).collect::<Vec<char>>() == REVERSE,
                 Direction::Down => self.pos.y + 3 < self.columns.len()
                     && self.pos.x >= 3
-                    && self.columns[self.pos.y..self.pos.y + 4].iter().enumerate().map(|(count, column)| column[self.pos.x - count]).collect::<Vec<char>>() == needle,
+                    && self.columns[self.pos.y..self.pos.y + 4].iter().enumerate().map(|(count, column)| column[self.pos.x - count]).collect::<Vec<char>>() == NEEDLE,
                 Direction::Right => self.pos.y + 3 < self.columns.len()
                     && self.pos.x + 3 < self.columns[self.pos.y].len()
-                    && self.columns[self.pos.y..self.pos.y + 4].iter().enumerate().map(|(count, column)| column[self.pos.x + count]).collect::<Vec<char>>() == needle,
+                    && self.columns[self.pos.y..self.pos.y + 4].iter().enumerate().map(|(count, column)| column[self.pos.x + count]).collect::<Vec<char>>() == NEEDLE,
                 Direction::Left => self.pos.y >= 3
                     && self.pos.x >= 3
-                    && self.columns[self.pos.y - 3..=self.pos.y].iter().enumerate().map(|(count, column)| column[self.pos.x - count]).collect::<Vec<char>>() == needle,
+                    && self.columns[self.pos.y - 3..=self.pos.y].iter().enumerate().map(|(count, column)| column[self.pos.x - count]).collect::<Vec<char>>() == REVERSE,
             },
         };
 
@@ -108,12 +109,70 @@ impl Word {
     }
 }
 
+pub struct Part2 {
+    columns: Vec<Vec<char>>,
+    pos: Position,
+}
+
+impl From<Part1> for Part2 {
+    fn from(part1: Part1) -> Part2 {
+        Part2 {
+            columns: part1.columns,
+            pos: Position::default(),
+        }
+    }
+}
+
+impl Part2 {
+    fn down<F>(&self, f: F) -> Result<[char; 3], Box<dyn std::error::Error>>
+    where
+        F: Fn(Position, usize) -> usize
+    {
+        self.columns[self.pos.y..self.pos.y + 3].iter()
+            .enumerate()
+            .map(|(count, column)| column[f(self.pos, count)])
+            .collect::<Vec<char>>()
+            .try_into()
+            .map_err(|_| "failed to convert".into())
+    }
+
+    fn scan(&self) -> Result<bool, Box<dyn std::error::Error>> {
+        let right = self.down(|pos, count| pos.x + count)?;
+        let down = self.down(|pos, count| (pos.x + 2) - count)?;
+
+        println!("[scan] right={:?}, down={:?}, pos={:?}", right, down, self.pos);
+
+        Ok(matches!(right, MAS_NEEDLE | MAS_REVERSE) && matches!(down, MAS_NEEDLE | MAS_REVERSE))
+    }
+
+    pub fn calculate(&mut self) -> Result<usize, Box<dyn std::error::Error>> {
+        let mut count = 0;
+
+        while self.pos.y < self.columns.len() - 2 {
+            if self.scan()? {
+                count += 1;
+            }
+
+            if self.pos.x >= self.columns[self.pos.y].len() - 3 {
+                self.pos.x = 0;
+                self.pos.y += 1;
+            } else {
+                self.pos.x += 1;
+            }
+        }
+
+        Ok(count)
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut word = Word::new("test2.txt")?;
+    let mut part1 = Part1::new("input.txt")?;
 
-    // TODO: this should be 18 but we get 12
+    println!("part1: {}", part1.calculate());
 
-    println!("calculate: {}", word.calculate());
+    let mut part2 = Part2::from(part1);
+
+    println!("part2: {}", part2.calculate()?);
 
     Ok(())
 }
